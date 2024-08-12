812aea933b7847c77dd707c743ff2ca4b4ee912e
KyleMotill <Kylemotill@gmail.com>
2024-08-12 12:22:11 -0400

    added some java script

--- html_1/app.js
+++ html_1/app.js
@@ -1,7 +1,29 @@
 // alert("anything");
-
-
+//! = not
+//|| = or
+//&& = and
 
 
 let firstName = 'kyle';
 console.log(firstName);
+
+let cart = document.getElementById("cart")
+cart.innerHTML = "Shop"
+
+let wood1 = 200;
+let wood2 = 300;
+let totalWood = wood1 + wood2;
+console.log(totalWood);
+
+
+let shelves = 500;
+let tables = 300;
+let chairs = 200;
+
+if (shelves > tables && shelves > chairs){
+    console.log("you have more shelves than anything")
+}else{
+console.log("we are good")
+};
+
+
